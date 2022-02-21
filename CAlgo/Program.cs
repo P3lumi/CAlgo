b6dccb4b01063c4256c119792def03c9f0e9545f
Pelumi <toriolaabosede@gmail.com>
2022-02-21 22:05:19 +0100

    done

--- CAlgo/Program.cs
+++ CAlgo/Program.cs
@@ -6,7 +6,29 @@ namespace CAlgo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            int[] inputA = { 1, 2, 3, 4, 5 };
+            int[] inputB = { 15, 25, 35 };
+            int[] inputC = { 8, 8 };
+
+            Console.WriteLine(Output(inputA));
+            Console.WriteLine(Output(inputB));
+            Console.WriteLine(Output(inputC));
         }
+
+        public static int Output(int[] arr)
+        {
+            int sum = 0;
+            foreach (var item in arr)
+            {
+                if ((item % 2) == 0)
+                    sum++;
+                if ((item % 2) != 0)
+                    sum += 3;
+                if (item == 8)
+                    sum += 5;
+            }
+            return sum;
+        }
+
     }
 }
